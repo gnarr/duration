e147072d993c941038294c4c53c835c8cc059cc0
Gunnar Cortes <gnarr.c@gmail.com>
2019-04-01 12:03:51 +0000

    Added TS lint and Travis testing

---
+++ src/duration.test.ts
@@ -0,0 +1,181 @@
+import { Duration } from "./duration";
+
+test("convert nanoseconds to microseconds", () => {
+  expect(new Duration("4000 nanoseconds").MicroSeconds).toBe(4);
+});
+
+test("convert microseconds to milliseconds", () => {
+  expect(new Duration("9000 microseconds").MilliSeconds).toBe(9);
+});
+
+test("convert milliseconds to seconds", () => {
+  expect(new Duration("6000 milliseconds").Seconds).toBe(6);
+});
+
+test("convert seconds to minutes", () => {
+  expect(new Duration("120 seconds").Minutes).toBe(2);
+});
+
+test("convert minutes to hours", () => {
+  expect(new Duration("360 minutes").Hours).toBe(6);
+});
+
+test("convert hours to days", () => {
+  expect(new Duration("72 hours").Days).toBe(3);
+});
+
+test("convert days to weeks", () => {
+  expect(new Duration("14 days").Weeks).toBe(2);
+});
+
+test("convert weeks to years", () => {
+  expect(new Duration("365 days").Years).toBe(1);
+});
+
+test("convert days to years", () => {
+  expect(new Duration("365 weeks").Years).toBe(7);
+});
+
+test("convert years to decades", () => {
+  expect(new Duration("30 years").Decades).toBe(3);
+});
+
+test("convert decades to centuries", () => {
+  expect(new Duration("40 decades").Centuries).toBe(4);
+});
+
+test("convert centuries to millenniums", () => {
+  expect(new Duration("50 centuries").Millenniums).toBe(5);
+});
+
+test("convert nanoseconds to millenniums", () => {
+  expect(new Duration("63072000000000000000 nanoseconds").Millenniums).toBe(2);
+});
+
+test("convert millenniums to nanoseconds", () => {
+  expect(new Duration("2 millenniums").NanoSeconds).toBe(63072000000000000000);
+});
+
+test("convert millenniums to centuries", () => {
+  expect(new Duration("2 millenniums").Centuries).toBe(20);
+});
+
+test("convert centuries to decades", () => {
+  expect(new Duration("60 decades").Centuries).toBe(6);
+});
+
+test("convert decades to years", () => {
+  expect(new Duration("20 years").Decades).toBe(2);
+});
+
+test("convert years to weeks", () => {
+  expect(new Duration("7 years").Weeks).toBe(365);
+});
+
+test("convert years to days", () => {
+  expect(new Duration("5 years").Days).toBe(1825);
+});
+
+test("convert weeks to days", () => {
+  expect(new Duration("5 weeks").Days).toBe(35);
+});
+
+test("convert days to hours", () => {
+  expect(new Duration("2 days").Hours).toBe(48);
+});
+
+test("convert hours to minutes", () => {
+  expect(new Duration("7 hours").Minutes).toBe(420);
+});
+
+test("convert minutes to seconds", () => {
+  expect(new Duration("3 minutes").Seconds).toBe(180);
+});
+
+test("convert seconds to milliseconds", () => {
+  expect(new Duration("7 seconds").MilliSeconds).toBe(7000);
+});
+
+test("convert milliseconds to microseconds", () => {
+  expect(new Duration("2 milliseconds").MicroSeconds).toBe(2000);
+});
+
+test("convert microseconds to nanoseconds", () => {
+  expect(new Duration("5 microseconds").NanoSeconds).toBe(5000);
+});
+
+test("convert floating point microseconds to nanoseconds", () => {
+  expect(new Duration("8.432 microseconds").NanoSeconds).toBe(8432);
+});
+
+test("convert floating point microseconds to milliseconds", () => {
+  expect(new Duration("0.432 microseconds").MilliSeconds).toBe(0.000432);
+});
+
+test("Convert to human readable form", () => {
+  expect(new Duration("840 seconds").toString()).toBe("14 minutes");
+});
+
+test("should fail on bad input", () => {
+  expect(() => new Duration("20 meconds")).toThrow();
+  expect(() => new Duration("10")).toThrow();
+  expect(() => new Duration("minute")).toThrow();
+});
+
+test("should not fail on good input", () => {
+  expect(() => new Duration("10488.469970703125 milliseconds")).not.toThrow();
+});
+
+test("should show input on error", () => {
+  expect(() => new Duration("10488,469970703125 milliseconds")).toThrowError(
+    "Error in input: '10488,469970703125 milliseconds'",
+  );
+});
+
+test("should create a new instance from nanoseconds", () => {
+  expect(Duration.fromNanoseconds(18000000).MilliSeconds).toBe(18);
+});
+
+test("should create a new instance from microseconds", () => {
+  expect(Duration.fromMicroSeconds(18000).MilliSeconds).toBe(18);
+});
+
+test("should create a new instance from milliseconds", () => {
+  expect(Duration.fromMilliseconds(18000).Seconds).toBe(18);
+});
+
+test("should create a new instance from seconds", () => {
+  expect(Duration.fromSeconds(120).Minutes).toBe(2);
+});
+
+test("should create a new instance from minutes", () => {
+  expect(Duration.fromMinutes(60).Seconds).toBe(3600);
+});
+
+test("should create a new instance from hours", () => {
+  expect(Duration.fromHours(48).Days).toBe(2);
+});
+
+test("should create a new instance from days", () => {
+  expect(Duration.fromDays(14).Weeks).toBe(2);
+});
+
+test("should create a new instance from weeks", () => {
+  expect(Duration.fromWeeks(10).Days).toBe(70);
+});
+
+test("should create a new instance from years", () => {
+  expect(Duration.fromYears(20).Decades).toBe(2);
+});
+
+test("should create a new instance from decades", () => {
+  expect(Duration.fromDecades(4).Years).toBe(40);
+});
+
+test("should create a new instance from centuries", () => {
+  expect(Duration.fromCenturies(12).Years).toBe(1200);
+});
+
+test("should create a new instance from millenniums", () => {
+  expect(Duration.fromMillenniums(2).Years).toBe(2000);
+});
---
+++ src/duration.ts
@@ -0,0 +1,262 @@
+export interface IDuration {
+  NanoSeconds: number;
+  MicroSeconds: number;
+  MilliSeconds: number;
+  Seconds: number;
+  Minutes: number;
+  Hours: number;
+  Days: number;
+  Weeks: number;
+  Years: number;
+  Decades: number;
+  Centuries: number;
+  Millenniums: number;
+}
+
+/**
+ * Duration can easily convert between units. E.g '7 Weeks' to Micro Seconds.
+ * Usage:
+ * const sevenWeeks = new Duration('7 Weeks');
+ * const milliseconds = sevenWeeks.MilliSeconds;
+ *
+ * @class Duration
+ */
+export class Duration implements IDuration {
+  get NanoSeconds() {
+    return this.MicroSeconds * 1000;
+  }
+  set NanoSeconds(nanoSeconds) {
+    this.MicroSeconds = nanoSeconds / 1000;
+  }
+  get MicroSeconds() {
+    return this.milliSeconds * 1000;
+  }
+  set MicroSeconds(microSeconds) {
+    this.milliSeconds = microSeconds / 1000;
+  }
+  get MilliSeconds() {
+    return this.milliSeconds;
+  }
+  set MilliSeconds(milliSeconds) {
+    this.milliSeconds = milliSeconds;
+  }
+  get Seconds() {
+    return this.MilliSeconds / 1000;
+  }
+  set Seconds(seconds) {
+    this.MilliSeconds = seconds * 1000;
+  }
+  get Minutes() {
+    return this.Seconds / 60;
+  }
+  set Minutes(minutes) {
+    this.Seconds = minutes * 60;
+  }
+  get Hours() {
+    return this.Minutes / 60;
+  }
+  set Hours(hours) {
+    this.Minutes = hours * 60;
+  }
+  get Days() {
+    return this.Hours / 24;
+  }
+  set Days(days) {
+    this.Hours = days * 24;
+  }
+  get Weeks() {
+    return this.Days / 7;
+  }
+  set Weeks(weeks) {
+    this.Days = weeks * 7;
+  }
+  get Years() {
+    return this.Days / 365;
+  }
+  set Years(years) {
+    this.Days = years * 365;
+  }
+  get Decades() {
+    return this.Years / 10;
+  }
+  set Decades(decades) {
+    this.Years = decades * 10;
+  }
+  get Centuries() {
+    return this.Decades / 10;
+  }
+  set Centuries(centuries) {
+    this.Decades = centuries * 10;
+  }
+  get Millenniums() {
+    return this.Centuries / 10;
+  }
+  set Millenniums(millenniums) {
+    this.Centuries = millenniums * 10;
+  }
+  public static fromNanoseconds(nanoseconds: number) {
+    const duration = new Duration();
+    duration.NanoSeconds = nanoseconds;
+    return duration;
+  }
+  public static fromMicroSeconds(microseconds: number) {
+    const duration = new Duration();
+    duration.MicroSeconds = microseconds;
+    return duration;
+  }
+  public static fromMilliseconds(milliSeconds: number) {
+    const duration = new Duration();
+    duration.MilliSeconds = milliSeconds;
+    return duration;
+  }
+  public static fromSeconds(seconds: number) {
+    const duration = new Duration();
+    duration.Seconds = seconds;
+    return duration;
+  }
+  public static fromMinutes(minutes: number) {
+    const duration = new Duration();
+    duration.Minutes = minutes;
+    return duration;
+  }
+  public static fromHours(hours: number) {
+    const duration = new Duration();
+    duration.Hours = hours;
+    return duration;
+  }
+  public static fromDays(days: number) {
+    const duration = new Duration();
+    duration.Days = days;
+    return duration;
+  }
+  public static fromWeeks(weeks: number) {
+    const duration = new Duration();
+    duration.Weeks = weeks;
+    return duration;
+  }
+  public static fromYears(years: number) {
+    const duration = new Duration();
+    duration.Years = years;
+    return duration;
+  }
+  public static fromDecades(decades: number) {
+    const duration = new Duration();
+    duration.Decades = decades;
+    return duration;
+  }
+  public static fromCenturies(centuries: number) {
+    const duration = new Duration();
+    duration.Centuries = centuries;
+    return duration;
+  }
+  public static fromMillenniums(millenniums: number) {
+    const duration = new Duration();
+    duration.Millenniums = millenniums;
+    return duration;
+  }
+  public static getters() {
+    return [
+      "NanoSeconds",
+      "MicroSeconds",
+      "MilliSeconds",
+      "Seconds",
+      "Minutes",
+      "Hours",
+      "Days",
+      "Weeks",
+      "Years",
+      "Decades",
+      "Centuries",
+      "Millenniums",
+    ];
+  }
+
+  private static isSingular(value: number) {
+    return value % 100 !== 11 && value % 10 === 1;
+  }
+  private static toSingular(value: string) {
+    return value.replace(/ies$/, "y").replace(/s$/, "");
+  }
+  private milliSeconds: number;
+  public constructor(input?: string) {
+    this.milliSeconds = 0;
+    if (!input) {
+      return;
+    }
+    const parts = input.split(/([0-9]+\.?[0-9]*)/);
+    const n = Number(parts[1]);
+    if (isNaN(n) || parts[2].length === 0) {
+      throw new Error(`Error in input: '${input}'`);
+    }
+    const unit = parts[2].trim().toLowerCase();
+    switch (unit) {
+      case "nanosecond":
+      case "nanoseconds":
+        this.NanoSeconds = n;
+        return;
+      case "microsecond":
+      case "microseconds":
+        this.MicroSeconds = n;
+        return;
+      case "millisecond":
+      case "milliseconds":
+        this.MilliSeconds = n;
+        return;
+      case "second":
+      case "seconds":
+        this.Seconds = n;
+        return;
+      case "minute":
+      case "minutes":
+        this.Minutes = n;
+        return;
+      case "hour":
+      case "hours":
+        this.Hours = n;
+        return;
+      case "day":
+      case "days":
+        this.Days = n;
+        return;
+      case "week":
+      case "weeks":
+        this.Weeks = n;
+        return;
+      case "year":
+      case "years":
+        this.Years = n;
+        return;
+      case "decade":
+      case "decades":
+        this.Decades = n;
+        return;
+      case "century":
+      case "centuries":
+        this.Centuries = n;
+        return;
+      case "millennium":
+      case "millenniums":
+      case "millennia":
+        this.Millenniums = n;
+        return;
+      default:
+        throw new Error(`Error in input: '${input}'`);
+    }
+  }
+
+  public toString() {
+    for (const getter of Duration.getters().reverse()) {
+      const test = this[getter as (keyof IDuration)];
+      if (test >= 1) {
+        const unit = Duration.isSingular(test)
+          ? Duration.toSingular(getter)
+          : getter;
+        const formatted = test.toLocaleString(undefined, {
+          maximumFractionDigits: 2,
+        });
+        return `${formatted} ${unit.toLowerCase()}`;
+      }
+    }
+    return `${this.MicroSeconds} microseconds`;
+  }
+}
